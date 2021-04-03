c5eacf3bedfa9d6555ce92e40486ee3384aeb1ca
rahulchaudhary777 <ch.rahuljaat777@gmail.com>
2021-04-03 08:17:11 +0530

    using for_else condition

---
+++ Prime_numbers_in_an_Interval.py
@@ -0,0 +1,10 @@
+a = int(input("enter lowercase "))
+b = int(input('enter upper number '))
+
+print(f'prime numbers between {a} and {b} are')
+for i in range(a,b+1):
+    for j in range(2,i):
+        if i%j==0:
+            break
+    else:
+        print(i)
