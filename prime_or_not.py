8e0151527bf67a9f30a64dd6a9786b53f6a92d0a
rahulchaudhary777 <ch.rahuljaat777@gmail.com>
2021-04-03 09:01:40 +0530

    prime number using for loop

--- prime_or_not.py
+++ prime_or_not.py
@@ -8,13 +8,4 @@ for i in range(2,a):
         print('number is prime')
         break
 
-# without loop
-
-a = int(input('enter a number '))
-
-if a%1==0 and a%2!=0:
-    print("number is prime ")
-else:
-    print('number is not prime ')
-
 
